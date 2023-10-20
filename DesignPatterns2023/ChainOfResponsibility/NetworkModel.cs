f4bdebce4c668ebb25d15a7234acc46844cfb476
DatTranLK <97006331+DatTranLK@users.noreply.github.com>
2023-10-20 15:36:08 +0700

    Dat: Chain Of Responsibility Pattern

---
+++ DesignPatterns2023/ChainOfResponsibility/NetworkModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns2023.ChainOfResponsibility
+{
+    public class NetworkModel
+    {
+        public string IP { get; set; }
+        public bool Success { get; set; }
+        public NetworkModel(string ip, bool success)
+        {
+            this.IP = ip;
+            this.Success = success;
+        }
+    }
+}
---
+++ DesignPatterns2023/ChainOfResponsibility/SendARP.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DesignPatterns2023.ChainOfResponsibility
+{
+    public class SendARP : IChain
+    {
+        private IChain next;
+        public void SendRequest(NetworkModel request)
+        {
+            if (request.Success == false)
+            {
+                Console.WriteLine("Send ARP failed");
+                if (next != null)
+                {
+                    next.SendRequest(request);
+                }
+                else
+                {
+                    Console.WriteLine("Network transmission failed!!!! Terminating");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Send ARP Success");
+            }
+        }
+
+        public void SetNext(IChain nextChain)
+        {
+            this.next = nextChain;
+        }
+    }
+}
