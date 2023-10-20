c61144a458715f12fb582d39747143ce0098cb4b
DatTranLK <97006331+DatTranLK@users.noreply.github.com>
2023-10-20 16:26:26 +0700

    Dat: Strategy Pattern

---
+++ DesignPatterns2023/Strategy Pattern/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns2023.Strategy_Pattern
+{
+    public class Context
+    {
+        IStrategy strategy;
+        public Context(IStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
+        public void ExecuteStrategy()
+        {
+            strategy.Execute();
+        }
+    }
+}
---
+++ DesignPatterns2023/Strategy Pattern/DNS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns2023.Strategy_Pattern
+{
+    public class DNS : IStrategy
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Sending DNS request!");
+        }
+    }
+}
