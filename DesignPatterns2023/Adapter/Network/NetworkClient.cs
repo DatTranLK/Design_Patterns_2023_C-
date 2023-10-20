74c6d7d453aa4192d0a63e349060b67df70f6283
DatTranLK <97006331+DatTranLK@users.noreply.github.com>
2023-10-20 08:57:28 +0700

    Dat: Adapter Pattern

---
+++ DesignPatterns2023/Adapter/Network/NetworkClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns2023.Adapter.Network
+{
+    public class NetworkClient : INetworkClient
+    {
+        public void SendRequest(string ipAddress)
+        {
+            Console.WriteLine("Network client request sent to IP: " + ipAddress);
+        }
+    }
+}
--- DesignPatterns2023/Program.cs
+++ DesignPatterns2023/Program.cs
@@ -1,11 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using DesignPatterns2023.Adapter;
+using DesignPatterns2023.Adapter.DataProcessor;
+using DesignPatterns2023.Adapter.Network;
 using DesignPatterns2023.Facade;
 using DesignPatterns2023.Factory.NetworkFactory;
 using DesignPatterns2023.Factory.NetworkUtility;
 using DesignPatterns2023.Singleton;
-using System.Net.NetworkInformation;
+
 
 #region Singleton
 /*Singleton object1 = Singleton.Instance();
@@ -29,7 +32,18 @@ arp.SendRequest("1.1.1", 1);*/
 #endregion
 
 #region Facade Pattern
-NetworkFacade networkFacade = new NetworkFacade("1.1.1.1", "UDP");
+/*NetworkFacade networkFacade = new NetworkFacade("1.1.1.1", "UDP");
 networkFacade.BuildNetworkLayer();
-networkFacade.SendPacketOverNetwork();
+networkFacade.SendPacketOverNetwork();*/
+#endregion
+
+#region Adapter Pattern
+INetworkClient network = new NetworkClient();
+network.SendRequest("1.1.1.1");
+
+IDataProcessor dataProc = new DataProcessor();
+dataProc.SendNetworkRequest("2.2.2.2", "23465gfhghg");
+
+NetworkAdapter adapter = new NetworkAdapter(dataProc);
+adapter.SendRequest("8.8.8.8");
 #endregion
